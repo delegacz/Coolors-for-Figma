60feffb84c3cdd1e4c331632bae745e7c5f5d257
Mateusz <mat.delegacz@gmail.com>
2020-07-08 20:08:23 +0100

    added url validation

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# Webpack 
+# Coolors for Figma
 
-<img src="../_screenshots/webpack.png" width="400" />
 
-Creates rectangles. Demonstrates bundling plugin code using Webpack.
+Simple Figma plugin creating colour pallete from coolors generated url
 
 The main plugin code is in `src/code.ts`. The HTML for the UI is in
 `src/ui.html`, while the embedded JavaScript is in `src/ui.ts`.
--- src/code.ts
+++ src/code.ts
@@ -1,44 +1,51 @@
 figma.showUI(__html__)
 figma.ui.resize(600,240)
 
+
 figma.ui.onmessage = msg => {
+  //
   if (msg.type === 'coolors-url') {
-    const nodes = []
-    const UrlFromForm = msg.url;
-    const regEx = /!@#(){}<>~.,;':/;
-    if (UrlFromForm.match(regEx)) {
-        console.log('regex triggered')
-       document.getElementById('error-message').classList.add('active')
-    }
-    else {
-    let arrOfCollors = UrlFromForm.split('-');
-    //set starting position of the rectange to 0
-    let pos = 0;
 
-    arrOfCollors.forEach(element => {
-      let rgbColor = hexToRgb(element);
-      const rect = figma.createRectangle()
-      rect.x = pos += 120
-      console.log(hexToRgb(element))
-      rect.fills = [{type: 'SOLID', color: hexToRgb(element)}]
-      figma.currentPage.appendChild(rect)
-      nodes.push(rect)
-    }, pos);
+    let urlForSamleTesting = msg.url;
+    let urlSample = urlForSamleTesting.substring(0,18);
+    console.log(urlSample);
+    const regexTest = /^([a-f0-9]{6})\-([a-f0-9]{6})\-([a-f0-9]{4})/
     
-    function hexToRgb(hex) {
-      var result = /([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? {
-        r: (parseInt(result[1], 16)*1)/255,
-        g: (parseInt(result[2], 16)*1)/255,
-        b: (parseInt(result[3], 16)*1)/255
-      } : null;
+    if(urlSample == 'https://coolors.co' || regexTest.test(urlSample)) {
+      console.log('Valid');
+      const nodes = []
+      const UrlFromForm = msg.url;
+      let arrOfCollors = UrlFromForm.split('-');
+      //set starting position of the rectange to 0
+      let pos = 0;
+      arrOfCollors.forEach(element => {
+        let rgbColor = hexToRgb(element);
+        const rect = figma.createRectangle()
+        rect.x = pos += 120
+        console.log(hexToRgb(element))
+        rect.fills = [{type: 'SOLID', color: hexToRgb(element)}]
+        figma.currentPage.appendChild(rect)
+        nodes.push(rect)
+      }, pos);
       
+      function hexToRgb(hex) {
+        var result = /([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+          r: (parseInt(result[1], 16)*1)/255,
+          g: (parseInt(result[2], 16)*1)/255,
+          b: (parseInt(result[3], 16)*1)/255
+        } : null;
+      }
+      figma.currentPage.selection = nodes
+      figma.viewport.scrollAndZoomIntoView(nodes)
+      figma.closePlugin()
     }
+    else{
+      
+      console.log('invalid'); 
+      figma.closePlugin()
+    }
+   
   }
-    
-
-    figma.currentPage.selection = nodes
-    figma.viewport.scrollAndZoomIntoView(nodes)
-  }
-  figma.closePlugin()
+  
 }
